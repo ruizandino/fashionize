3af4d6ac88d1658d8c0874cdfdcade5c4eb0dce7
ruizandino <talcahuano85@gmail.com>
2020-10-20 19:29:29 -0300

    Agregué rutas y archivos .ejs

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,12 @@ router.get('/productDetail', function(req, res) {
   res.render('productDetail');
 });
 
+router.get('/register', function(req, res) {
+  res.render('register');
+});
+
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
 module.exports = router;
